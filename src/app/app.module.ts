555679e4c60e9bf2f6d86f08de1fbcc0bb8d46a3
Hortência Côrtes <hortencia.cortes@gmail.com>
2024-09-27 16:07:58 -0300

    ADD: game card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './shared/components/menu/menu.component';
+import { GameCardComponent } from './shared/components/game-card/game-card.component';
+import { CardLabelComponent } from './shared/components/game-card/card-label/card-label.component';
+import { CardPricingComponent } from './shared/components/game-card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
+    GameCardComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
