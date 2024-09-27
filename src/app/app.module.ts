7559f5ec1d86a3c32c2e8f17cf119cab87894845
Hortência Côrtes <hortencia.cortes@gmail.com>
2024-09-27 15:15:20 -0300

    ADD: component menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
